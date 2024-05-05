8426fee55ff0e5b060ccbb858349c1bec9d25d6e
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-05-05 12:55:51 +0200

    Implemented header component and header css module for styling header

---
+++ app/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import styles from "@/app/components/header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Image src="/images/logo.png" width={64} height={64} alt="coffee logo" />
+      <h1>The Columbiana Brew</h1>
+    </header>
+  );
+};
+
+export default Header;
