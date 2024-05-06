363e3a6fde193ef4e27474a36fda9073edd9f489
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-05-06 19:15:12 +0200

    Implemented postgres vercel seed logic

---
+++ app/lib/data/placeholder-data.js
@@ -0,0 +1,79 @@
+const orders = [
+  {
+    name: "Test order",
+    phone: "444-555-666",
+    zipCode: "ZAR555",
+  },
+];
+
+const items = [
+  {
+    itemId: "coffee",
+    imageId: "coffee",
+    title: "Drip Coffee",
+    price: 1,
+    description: "",
+    salePrice: 0,
+  },
+  {
+    itemId: "cookie",
+    imageId: "cookie",
+    title: "Cookie",
+    price: 0.99,
+    description: "May contain nuts.",
+    salePrice: 0.5,
+  },
+  {
+    itemId: "croissant",
+    imageId: "croissant",
+    title: "Croissant",
+    price: 1.5,
+  },
+  {
+    itemId: "cupcake",
+    imageId: "cupcake",
+    title: "Cupcake",
+    price: 2.5,
+  },
+  {
+    itemId: "french-press",
+    imageId: "french-press",
+    title: "French Press",
+    price: 1,
+  },
+  {
+    itemId: "iced-coffee",
+    imageId: "iced-coffee",
+    title: "Iced Coffee",
+    price: 1.25,
+  },
+  {
+    itemId: "latte",
+    imageId: "latte",
+    title: "Latte",
+    price: 1.75,
+  },
+  {
+    itemId: "milk",
+    imageId: "milk",
+    title: "Chocolate Milk",
+    price: 0.5,
+  },
+  {
+    itemId: "sandwich",
+    imageId: "sandwich",
+    title: "Sandwich",
+    price: 6,
+  },
+  {
+    itemId: "tea",
+    imageId: "tea",
+    title: "Tea",
+    price: 1,
+  },
+];
+
+module.exports = {
+  items,
+  orders,
+};
---
+++ scripts/seed.js
@@ -0,0 +1,98 @@
+const { db } = require("@vercel/postgres");
+const { items, orders } = require("../app/lib/placeholder-data.js");
+
+async function seedItems(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS items (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        item_id VARCHAR(255),
+        image_id VARCHAR(255),
+        title  VARCHAR(255),
+        price DECIMAL(12,2),
+        description VARCHAR(255),
+        sale_price DECIMAL(12,2),
+      );
+    `;
+
+    console.log(`Created "items" table`);
+
+    // Insert data into the "items" table
+    const insertedItems = await Promise.all(
+      items.map(async (item) => {
+        return client.sql`
+        INSERT INTO users (item_id, image_id, title, price, description, sale_price)
+        VALUES (${item.item_id}, ${item.image_id}, ${item.title}, ${item.price}, ${item.description}, ${item.sale_price})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
+    );
+
+    console.log(`Seeded ${insertedItems.length} items`);
+
+    return {
+      createTable,
+      items: insertedItems,
+    };
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    throw error;
+  }
+}
+
+async function seedOrders(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    // Create the "orders" table if it doesn't exist
+    const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS orders (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    zip_code VARCHAR(255),
+    item_id VARCHAR(255),
+  );
+`;
+
+    console.log(`Created "orders" table`);
+
+    // Insert data into the "orders" table
+    const insertedOrders = await Promise.all(
+      orders.map(
+        (order) => client.sql`
+        INSERT INTO orders (name, phone, zip_code)
+        VALUES (${order.name}, ${order.phone}, ${order.zip_code})
+        ON CONFLICT (id) DO NOTHING;
+      `
+      )
+    );
+
+    console.log(`Seeded ${insertedOrders.length} orders`);
+
+    return {
+      createTable,
+      orders: insertedOrders,
+    };
+  } catch (error) {
+    console.error("Error seeding orders:", error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  await seedItems(client);
+  await seedOrders(client);
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error(
+    "An error occurred while attempting to seed the database:",
+    err
+  );
+});
