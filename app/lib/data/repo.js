6d5d40e39a93eab446f5529651036e5a0f1698cb
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-05-07 14:28:04 +0200

    refactored initial data

---
+++ app/lib/data/repo.js
@@ -0,0 +1,23 @@
+import { sql } from "@vercel/postgres";
+
+export async function fetchItems() {
+  // Add noStore() here to prevent the response from being cached.
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const data = (await sql)`SELECT * FROM items`;
+
+    // console.log('Data fetch completed after 3 seconds.');
+
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch items data.");
+  }
+}
--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/app/components/header";
 import Home from "@/app/components/home";
-import { items } from "@/app/lib/items";
+//import { items } from "@/app/lib/items";
+import { fetchItems } from "@/app/lib/data/repo";
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const items = await fetchItems();
   return (
     <div className={styles.container}>
       <Header />
